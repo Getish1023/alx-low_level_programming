da95fefc2591ae949b039aa9379490a8d000efcf
Getish1023 <getishtesfaye23@gmail.com>
2023-03-20 11:27:04 -0700

    compile

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
 /**
  * main - Entry point
- * Return: Always  (Success)
+ * Return: Alwys  (Success)
  */
 int main(void)
 {
-	fwrite(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
