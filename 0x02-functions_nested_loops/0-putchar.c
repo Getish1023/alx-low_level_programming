1fccf7399620160c4c28fc45f103b6b1e90bef94
Getish1023 <getishtesfaye23@gmail.com>
2023-03-22 01:54:56 -0700

    putcher

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
